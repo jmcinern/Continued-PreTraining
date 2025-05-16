085e8c2cb123fbb703122217a3b5fc28741013c1
joseph <jmcinern@tcd.ie>
2025-05-16 15:41:57 +0100

    run train

--- qwen_mini_cpt.sh
+++ qwen_mini_cpt.sh
@@ -13,5 +13,5 @@
 module load python3/3.10.5/gcc-9.3.0 # availible python
 source /mnt/scratch2/users/40460549/cpt-dail/myenv_new/bin/activate
 cd $SLURM_SUBMIT_DIR                     # ensure we’re in the project dir
-srun python eval_CPT_irish.py
+srun python mini_CPT.py #eval_CPT_irish.py
                # run your script
