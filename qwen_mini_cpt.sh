15f282c532f94ffcaa2f7b9f668f863fefa620d2
joseph <jmcinern@tcd.ie>
2025-05-16 17:31:48 +0100

    a100 requested

--- qwen_mini_cpt.sh
+++ qwen_mini_cpt.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-#SBATCH --job-name=mini_qwen_test           # job name
-#SBATCH --output=qwen_mini_cpt_%j.out      # STDOUT → hello_slurm_<jobid>.out
-#SBATCH --error=qwen_mini_cpt_%j.err       # STDERR → hello_slurm_<jobid>.err
-#SBATCH --time=12:00:00                  # time
-#SBATCH --partition=k2-gpu-v100            # partition
+#SBATCH --job-name=mini_qwen_a100
+#SBATCH --output=qwen_mini_cpt_%j.out
+#SBATCH --error=qwen_mini_cpt_%j.err
+#SBATCH --time=20:00:00
+#SBATCH --partition=k2-gpu-a100
 #SBATCH --gres=gpu:1
-#SBATCH --ntasks=1                       # single‐process job
-#SBATCH --cpus-per-task=4                # one CPU
-#SBATCH --mem=16G                 # 1 GB RAM
-#SBATCH --mail-type=ALL       # email on start, end, or fail
-#SBATCH --mail-user=josephmcinerney7575@gmail.com 
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --mem=32G
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=josephmcinerney7575@gmail.com
 module load python3/3.10.5/gcc-9.3.0 # availible python
 source /mnt/scratch2/users/40460549/cpt-dail/myenv_new/bin/activate
 cd $SLURM_SUBMIT_DIR                     # ensure we’re in the project dir
