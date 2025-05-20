ecc8690bc3c66512e1dde30c8e6856b8dc5226e8
joseph <jmcinern@tcd.ie>
2025-05-20 11:08:41 +0100

    switched to interactive HPC session for debugging

--- qwen_mini_cpt.sh
+++ qwen_mini_cpt.sh
@@ -3,7 +3,7 @@
 #SBATCH --output=qwen_mini_cpt_%j.out
 #SBATCH --error=qwen_mini_cpt_%j.err
 #SBATCH --time=00:10:00
-#SBATCH --partition=k2-gpu-a100   #k2-gpu-a100 # view gpu partitions: sinfo -o "%P %G %D %C %t %N" , sinfo | grep gpu
+#SBATCH --partition=k2-gpu-interactive   #k2-gpu-v100, k2-gpu-interactive # view gpu partitions: sinfo -o "%P %G %D %C %t %N" , sinfo | grep gpu
 #SBATCH --gres=gpu:1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
