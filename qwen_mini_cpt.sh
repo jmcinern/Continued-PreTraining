a583f0d74a6f4f22d3c87d99917896cb6cf78aa2
joseph <jmcinern@tcd.ie>
2025-07-01 11:16:25 +0100

    requesting 2 GPUs to mirror real training

--- qwen_mini_cpt.sh
+++ qwen_mini_cpt.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-#SBATCH --job-name=mini_qwen_a100
+#SBATCH --job-name=CPT_JM
 #SBATCH --output=./out/qwen_mini_cpt_%j.out
 #SBATCH --error=./err/qwen_mini_cpt_%j.err
 #SBATCH --time=000:20:00
-#SBATCH --partition=k2-gpu-v100   #k2-gpu-v100, k2-gpu-interactive
-# srun --partition=k2-gpu-interactive --gres=gpu:2 --time=1:30:00 --pty bash 
-# view gpu partitions: sinfo -o "%P %G %D %C %t %N"  sinfo | grep gpu
-#SBATCH --gres=gpu:1
+#SBATCH --partition=k2-gpu-v100  
+#SBATCH --gres=gpu:2
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=4
 #SBATCH --mem=256G
@@ -19,9 +17,15 @@ source /mnt/scratch2/users/40460549/cpt-dail/myenv_new/bin/activate
 pip install --no-cache-dir -r "requirements.txt"
 export WANDB_API_KEY = "2dab6162cdfdc1b28724ac4ce95bb597d7f85994"
 cd $SLURM_SUBMIT_DIR                     # ensure we’re in the project dir
+deepspeed --num_gpus=2 mini_CPT.py
+
+
+# OLD COMMANDS
+# srun --partition=k2-gpu-interactive --gres=gpu:2 --time=1:30:00 --pty bash 
+# view gpu partitions: sinfo -o "%P %G %D %C %t %N"  sinfo | grep gpu
 #srun python eval_base_qwen_mini.py 
 #srun python mini_CPT.py
 #accelerate launch \
   #--num_processes 2 \
  # --mixed_precision no \
-deepspeed --num_gpus=2 mini_CPT.py
+  #k2-gpu-v100, k2-gpu-interactive
